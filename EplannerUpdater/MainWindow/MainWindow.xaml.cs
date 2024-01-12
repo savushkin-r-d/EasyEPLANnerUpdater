7957c4c2f2227543dfdd6dfbe02d92da801bcc37
KirillGutyrchik <gutyrcikkirill@gmail.com>
2024-01-12 09:11:00 +0300

    Add hyperlink help buttons (#20)

--- EplannerUpdater/MainWindow/MainWindow.xaml.cs
+++ EplannerUpdater/MainWindow/MainWindow.xaml.cs
@@ -238,4 +238,13 @@ public partial class MainWindow : Window
 
     private void UpdateBTODescriptionButton_Click(object sender, RoutedEventArgs e)
         => Model.UpdateBTODescription();
+
+    private void HelpToWorkApp_Click(object sender, RoutedEventArgs e)
+    {
+        Process.Start(
+            new ProcessStartInfo($"{Settings.Default.ReadMeUrl}использование-приложения")
+            {
+                UseShellExecute = true
+            });
+    }
 }
